73fe078c1220b45117ed733c58f2d34c05194a8e
Pauline Moraa <pmoraa431@gmail.com>
2025-05-25 04:02:54 +0300

    CRUD operation in sqlalchemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -12,10 +12,67 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
+    __table_args__ = (
+        PrimaryKeyConstraint('id', name='id_pk'),
+        UniqueConstraint('email', name='unique_email'),
+        CheckConstraint('grade BETWEEN 1 AND 12', name='grade_between_1_and_12')
+    )
+    Index('index_name', 'name')
 
-    id = Column(Integer(), primary_key=True)
+    id = Column(Integer())
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday =Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: "\
+                + f"{self.name}, "\
+                + f"{self.email}, "\
+                + f"Grade {self.grade}"
+    
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+albert_einstein = Student(
+    name='Albert Einstein',
+    email='albert.einstein@zurich.edu',
+    grade=6,
+    birthday=datetime(
+        year=1879, 
+        month=3,
+        day=14
+    ),
+    
+    )
+
+alan_turing = Student(
+        name="Alan Turing",
+        email="alan.turing@sherborne.edu",
+        grade=11,
+        birthday=datetime(
+            year=1912,
+            month=6,
+            day=23
+        ),
+    )
+
+session.bulk_save_objects([albert_einstein, alan_turing])
+session.commit()
+
+query = session.query(
+        Student).filter(
+            Student.name == "Albert Einstein")
+
+query.delete()
+
+albert_einstein = query.first()
+
+print(albert_einstein)
+
